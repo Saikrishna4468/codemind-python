3aed338fedac88f44dbacb0f90c301512e0a9e51
Rajana Saikrishna <102219617+Saikrishna4468@users.noreply.github.com>
2022-09-12 21:56:59 +0530

    for _ in range(int(input())):     n=str(input())     print(int(n,2))

--- Binary_to_Decimal.py
+++ Binary_to_Decimal.py
@@ -1,8 +1,3 @@
 for _ in range(int(input())):
-    a=reversed(input())
-    c=d=0
-    for i in a:
-        if i=="1":
-            c=c+2**d
-        d+=1
-    print(c)
+    n=str(input())
+    print(int(n,2))
