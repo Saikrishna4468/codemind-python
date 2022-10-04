fd2fb42221c530616fa097e6225264458eb1e1ad
Rajana Saikrishna <102219617+Saikrishna4468@users.noreply.github.com>
2022-10-04 13:08:55 +0530

    l=int(input()) for i in range(l):     n=input().split()     for i in n:         if ((i)).isnumeric():             print("True")         else:             print("False")

---
+++ String_decimal_.py
@@ -0,0 +1,9 @@
+l=int(input())
+for i in range(l):
+    n=input().split()
+    for i in n:
+        if ((i)).isnumeric():
+            print("True")
+        else:
+            print("False")
+            
